df72d4586022d99f0e7fd12ee657907f84051736
prabhat vyas <vyas.prabhat@gmail.com>
2026-08-20 10:53:03 +0530

    Remove incomplete image data

--- assets.js
+++
@@ -1,6 +0,0 @@
-window.JAIPUR_SAFARI_IMAGES = {
-  "innova_crysta": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4...",
-  "honda_city": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4...",
-  "force_urbania": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4...",
-  "urbania_interior": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4..."
-};
