1ec5b64e3199639e844f3da74aaecad3924c6589
prabhat vyas <vyas.prabhat@gmail.com>
2026-08-20 10:52:37 +0530

    Prepare Jaipur Safari vehicle image data

--- assets.js
+++ assets.js
@@ -1,1 +1,6 @@
-window.JAIPUR_SAFARI_IMAGES = {};
+window.JAIPUR_SAFARI_IMAGES = {
+  "innova_crysta": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4...",
+  "honda_city": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4...",
+  "force_urbania": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4...",
+  "urbania_interior": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHh4aHj4..."
+};
